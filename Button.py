c517388bd20a4c1a126d3a4a27a79e55464f2905
SanGReaL53 <58322074+SanGReaL53@users.noreply.github.com>
2020-11-05 14:23:12 +0300

    Update Button.py

--- Button.py
+++ Button.py
@@ -1,9 +1,13 @@
 class Button:
+    
     def __init__(self):
         self.clicks = 0
+        
     def click(self):
         self.clicks += 1
+        
     def click_count(self):
         print(self.clicks)
+        
     def reset(self):
         self.clicks = 0
