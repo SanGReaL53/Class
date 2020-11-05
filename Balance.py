9178ad2e88d399337c2539421d5bae33c050382d
SanGReaL53 <58322074+SanGReaL53@users.noreply.github.com>
2020-11-05 14:19:51 +0300

    Add files via upload

---
+++ Balance.py
@@ -0,0 +1,20 @@
+class Balance:
+    def __init__(self):
+        self.right = 0
+        self.left = 0
+    def add_right(self, amount):
+        self.right += amount
+    def add_left(self, amount):
+        self.left += amount
+    def result(self):
+        if self.left == self.right:
+            print('=')
+        elif self.left > self.right:
+            print('L')
+        else:
+            print('R')
+new = Balance()
+new.add_right(8)
+new.add_left(6)
+new.add_left(1)
+new.result()
